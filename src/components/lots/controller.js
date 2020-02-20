8e6bb2da47bdcd6c1342fa1b21a3e1453bb6eae3
Danteisabck <berezhnoya23@gmail.com>
2020-02-20 17:18:53 +0200

    Implemented get route for lot list and lot

--- src/components/lots/controller.js
+++ src/components/lots/controller.js
@@ -1,4 +1,4 @@
-import { createLot } from './service';
+import { createLot, loadLots, loadLot, getLotsCount } from './service';
 
 export const create = async (req, res, next) => {
   const {
@@ -8,6 +8,44 @@ export const create = async (req, res, next) => {
 
   try {
     const lot = await createLot({ name, description, creator: id });
+
+    return res.json(lot);
+  } catch (e) {
+    return next(e);
+  }
+};
+
+export const getLots = async (req, res, next) => {
+  const params = {};
+  const page = +req.query.page || 1;
+  const limit = +req.query.limit || 10;
+
+  if (req.query.id) {
+    params.creator = req.query.id;
+  }
+
+  try {
+    const lots = await loadLots({ params, limit, page });
+    const lotsCount = await getLotsCount();
+
+    const data = {
+      items: lots,
+      currentPage: page,
+      pages: Math.ceil(lotsCount / page),
+      count: lotsCount,
+    };
+
+    return res.json(data);
+  } catch (e) {
+    return next(e);
+  }
+};
+
+export const getLot = async (req, res, next) => {
+  const lotId = req.params.lotId;
+
+  try {
+    const lot = await loadLot(lotId);
 
     return res.json(lot);
   } catch (e) {
--- src/components/lots/service.js
+++ src/components/lots/service.js
@@ -1,5 +1,13 @@
 import LotModel from './model';
 
-export const createLot = ({ name, description, creator }) => {
-  return LotModel.create({ name, description, creator });
-};
+export const createLot = ({ name, description, creator }) =>
+  LotModel.create({ name, description, creator });
+
+export const loadLots = ({ params, limit, page }) =>
+  LotModel.find(params)
+    .skip(limit * page - limit)
+    .limit(limit);
+
+export const loadLot = id => LotModel.find({ _id: id });
+
+export const getLotsCount = () => LotModel.countDocuments({});
